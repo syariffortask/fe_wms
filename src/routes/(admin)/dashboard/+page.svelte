25716ee8ec3e052846b6047f057d04d29b82adc0
IT CPI <syariffortask@gmail.com>
2025-10-21 11:36:55 +0700

    push readme

--- src/routes/(admin)/dashboard/+page.svelte
+++ src/routes/(admin)/dashboard/+page.svelte
@@ -4,5 +4,6 @@
 <!-- Untuk role admin -->
 
 <div class="flex mt-[-10px] flex-col h-full">
-  <h1>Dashboard</h1>
+  <h1 class="text-xl">Dashboard</h1><br>
+  <p>Sementara masih kosong, akan diisi nanti</p>
 </div>
